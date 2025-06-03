7877725351dba58fb7234b5e4a0556f739f4ddb5
wtb <aagknaag@hotmail.com>
2025-06-03 23:12:21 +0200

    Since AST is changed for figure renames two visualization stages are created instead of one

--- src/flexitex/main.py
+++ src/flexitex/main.py
@@ -12,19 +12,21 @@ from flexitex.generators.latex_generator import LatexGenerator
 from flexitex.generators.dot_generator import DotGenerator
 
 
-def run_main(config_path: str, debug: bool, visualize: bool):
+def run_main(config_path: str, debug: bool, visualize_original: bool, visualize_final: bool):
     config = Config(path=config_path)
 
-    processor = LatexProcessor(config.input_folder, config.input_main_file, debug=debug)
+    processor = LatexProcessor(
+        config.input_folder, config.input_main_file, debug=debug)
     ast = processor.parse()
 
-    if visualize:
+    if visualize_original:
         dot = DotGenerator()
         dot_str = dot.to_dot(ast, hide_before_document=True)
         graph = Source(dot_str, format="pdf")
-        graph.view(cleanup=True)
+        graph.view("original", cleanup=True)
 
-    graphics_mover = GraphicsMoveManager(config.input_folder, config.output_folder, config.output_figure_folder)
+    graphics_mover = GraphicsMoveManager(
+        config.input_folder, config.output_folder, config.output_figure_folder)
     ast = graphics_mover.detect_moves(ast)
 
     structure = Structure(
@@ -40,9 +42,16 @@ def run_main(config_path: str, debug: bool, visualize: bool):
     writer.write_all(files, clear_output=True)
     graphics_mover.move_files()
 
+    if visualize_final:
+        dot = DotGenerator()
+        dot_str = dot.to_dot(ast, hide_before_document=True)
+        graph = Source(dot_str, format="pdf")
+        graph.view("final", cleanup=True)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="FlexiTeX - LaTeX project style transformation tool")
+    parser = argparse.ArgumentParser(
+        description="FlexiTeX - LaTeX project style transformation tool")
     parser.add_argument(
         "-c", "--config", default="config.yml", help="Path to config YAML file"
     )
@@ -50,11 +59,15 @@ def main():
         "--debug", action="store_true", help="Enable debug output during parsing"
     )
     parser.add_argument(
-        "--visualize", action="store_true", help="Print Graphviz DOT of AST"
+        "-vo", "--visualize-original", action="store_true", help="Display Graphviz pdf of initial AST"
+    )
+    parser.add_argument(
+        "-vf", "--visualize-final", action="store_true", help="Display Graphviz pdf of final AST"
     )
 
     args = parser.parse_args()
-    run_main(args.config, args.debug, args.visualize)
+    run_main(args.config, args.debug,
+             args.visualize_original, args.visualize_final)
 
 
 if __name__ == "__main__":
